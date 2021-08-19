f9fc6b6c6b474838545ebbda1e5692c5a140f7ea
TokuyamaR <qpflapper.1007@gmail.com>
2021-08-19 15:04:30 +0900

    create BaseButton component

--- vue3-counter/src/App.vue
+++ vue3-counter/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <!-- <TheHeader text="My Counter" /> -->
   <TheHeader
     v-bind:text="Math.floor(Math.random() * 10) % 2 === 0 ? 'even' : 'odd'"
   />
+  <BaseButton @onClick="alertOnClick">+</BaseButton>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import BaseButton from "./components/BaseButton.vue";
 
 export default {
   components: {
     TheHeader,
+    BaseButton,
+  },
+  methods: {
+    alertOnClick() {
+      alert("Button Clicked!!!");
+    },
   },
 };
 </script>
