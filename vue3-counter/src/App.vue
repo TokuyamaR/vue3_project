8615ca9f640f0fb649bca2420e18cd15c5c1c574
TokuyamaR <qpflapper.1007@gmail.com>
2021-08-21 08:16:42 +0900

    add NumberInput components

--- vue3-counter/src/App.vue
+++ vue3-counter/src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <TheHeader
-    v-bind:text="Math.floor(Math.random() * 10) % 2 === 0 ? 'even' : 'odd'"
-  />
-  <BaseButton @onClick="alertOnClick">+</BaseButton>
-  <BaseButton @onClick="linkOnClick">Yahoo!</BaseButton>
+  <TheHeader text="My counter" />
+  <div>{{ count }}</div>
+  <BaseButton @onClick="minusOne">-</BaseButton>
+  <BaseButton @onClick="plusOne">+</BaseButton>
+
+  <NumberInput v-model.numberOnly="inputCount" />
+  <BaseButton @onClick="insertCount">insert</BaseButton>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import BaseButton from "./components/BaseButton.vue";
+import NumberInput from "./components/NumberInput.vue";
 
 export default {
   components: {
     TheHeader,
     BaseButton,
+    NumberInput,
+  },
+  data() {
+    return {
+      count: 0,
+      inputCount: 0,
+    };
   },
   methods: {
-    alertOnClick() {
-      alert("Button Clicked!!!");
+    plusOne() {
+      this.count++;
     },
-    linkOnClick() {
-      location.href = "https://www.yahoo.co.jp/";
+    minusOne() {
+      this.count--;
+    },
+    insertCount() {
+      this.count = this.inputCount;
     },
   },
 };
