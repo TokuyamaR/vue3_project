175589bd4a9bb22e4ac5d230617e40bca3cfafef
TokuyamaR <qpflapper.1007@gmail.com>
2021-08-22 09:54:10 +0900

    add ComputedApi sample

---
+++ vue3-counter/src/sample/ComputedSample.vue
@@ -0,0 +1,33 @@
+<template>
+  {{ immutable }}
+  {{ mutable }}
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const count = ref(1);
+    const immutable = computed(() => {
+      return count.value * 2;
+    });
+
+    const mutable = computed({
+      get: () => {
+        return count;
+      },
+      set: (newValue) => {
+        count.value = newValue.value;
+      },
+    });
+
+    console.log((immutable.value = 3));
+    console.log((mutable.value = 3));
+    return {
+      count,
+      immutable,
+      mutable,
+    };
+  },
+};
+</script>
