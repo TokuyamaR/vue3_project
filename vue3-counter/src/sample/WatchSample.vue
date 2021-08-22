5fd7e4430df3621b2eab7011ff1ebd0a0c6ba0f7
TokuyamaR <qpflapper.1007@gmail.com>
2021-08-22 10:28:47 +0900

    add watch api sample

---
+++ vue3-counter/src/sample/WatchSample.vue
@@ -0,0 +1,28 @@
+<template>
+  {{ count }}
+  <button @click="increment">Increment</button>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+export default {
+  setup() {
+    const count = ref(1);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    // countの変更を検知し実行される
+    watch(count, (next, prev) => {
+      console.log(next);
+      console.log(prev);
+    });
+
+    return {
+      count,
+      increment,
+    };
+  },
+};
+</script>
