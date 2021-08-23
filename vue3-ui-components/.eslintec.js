4ade715471f0b21fd01fe9a7068eab15bfc18387
TokuyamaR <qpflapper.1007@gmail.com>
2021-08-24 08:13:57 +0900

    add config settings

---
+++ vue3-ui-components/.eslintec.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['plugin: @typescript-eslint/recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    warnOnUnsupportedTypeScriptVersion: false,
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/no-v-html': 'error',
+    'vue/v-bind-style': 'off',
+    'vue/v-on-style': 'off',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        ignores: [
+          'keep-alive',
+          'transition',
+          'transition-group',
+          'component',
+          'slat',
+          'router-link',
+          'router-view',
+          'nuxt-link',
+        ],
+      },
+    ],
+  },
+};
