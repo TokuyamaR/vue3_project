80817ccf4043285f1379d7e4e1d29a896e0a3553
TokuyamaR <qpflapper.1007@gmail.com>
2021-09-02 23:14:40 +0900

    add InputText component

--- vue3-ui-components/src/App.vue
+++ vue3-ui-components/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <ButtonSample />
   <DialogSample />
+  <InputTextSample />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ButtonSample from './components/buttons/ButtonSample.vue';
 import DialogSample from './components/dialog/DialogSample.vue';
+import InputTextSample from './components/input/InputTextSample.vue';
 export default defineComponent({
   name: 'App',
   components: {
     ButtonSample,
     DialogSample,
+    InputTextSample,
   },
 });
 </script>
---
+++ vue3-ui-components/src/components/forms/InputTextSample.vue
@@ -0,0 +1,31 @@
+<template name="component-name">
+  <div>
+    <p class="mb-2 text-2xl">Forms</p>
+    <div class="w-80">
+      <InputText
+        v-model:value="inputTextValue"
+        placeholder="placeholder"
+        label="InputLabel with v-model"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import InputText from './InputText.vue';
+
+export default defineComponent({
+  name: 'InputTextSample',
+  components: {
+    InputText,
+  },
+  setup() {
+    const inputTextValue = ref('');
+
+    return {
+      inputTextValue,
+    };
+  },
+});
+</script>
